3627279822c37cbf6d96adefbf51e929228f5738
Emanuel Marinho <psic.emanuelmarinho@gmail.com>
2026-03-12 15:31:44 -0300

    Criação e aplicação da seed de categoria, REVISAR ANTES DO MERGE

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import {CreateUserController} from '@/controllers/CreateUserController'
-
-export const router = Router()
-
-const createUserController = new CreateUserController()
-
-router.post('/users', createUserController.handle)
-
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import {app} from '@/app'
-import {env} from '@/env'
-
-app.listen(env.PORT, () => {
-    console.log(`Servidor rodando na porta ${env.PORT}`);
-}); 
-
